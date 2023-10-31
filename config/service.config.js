a09c25a17ef8dd7625dac55762e11b6b9a9371a2
KNU-K <knukang334@gmail.com>
2023-10-31 20:59:19 +0900

    test : senior_facility controller 를 통해 받아옴
    test aws에서 해보기

---
+++ config/service.config.js
@@ -0,0 +1,5 @@
+const SeniorFacilitiesService = require("../services/senior_facilities.service");
+
+const seniorFacilitiesService = new SeniorFacilitiesService();
+
+module.exports = { seniorFacilitiesService };
