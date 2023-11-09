63040426bc4fc22a6e85d681cda9e00654ae9623
KNU-K <knukang334@gmail.com>
2023-11-10 00:54:44 +0900

    hofix : senior facility 선택된것 안된것 나눠서 get 받을 수 있게 수정
    hofix : senior facility 선택된것 안된것 나눠서 get 받을 수 있게 수정

--- controllers/senior_facilities.controller.js
+++ controllers/senior_facilities.controller.js
@@ -4,10 +4,33 @@ const find_all_facilities = (req, res, next) => {
     const result = seniorFacilitiesService.findAll();
     res.send(result);
   } catch (err) {
-    throw err;
+    next(err);
+  }
+};
+const find_facility_of_user = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await seniorFacilitiesService.findFacilityOfUser(id);
+    res.send(result);
+  } catch (err) {
+    next(err);
   }
 };
 
+const find_non_selected_facility_of_user = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await seniorFacilitiesService.findNonSelectedFacilityOfUser(
+      id
+    );
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = {
+  find_non_selected_facility_of_user: find_non_selected_facility_of_user,
+  find_facility_of_user: find_facility_of_user,
   find_facility: find_all_facilities,
 };
